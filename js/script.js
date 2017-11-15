4f4829bdbd1599e19d62adae72dd711959dae5d0
Vlad <hackerjavaVlad@gmail.com>
2017-11-15 19:01:14 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -50,9 +50,6 @@ setInterval(basket_remove , 10);
 
 
 
-
-
-
 // show basket
 var t_basket = documen_t.querySelector("#basket") , 
     box_basket = documen_t.querySelector("#box_basket");
@@ -222,6 +219,11 @@ var button_take_b = documen_t.getElementsByClassName("take_basket");
 
     function button_take_basket()
     {
+        var e_ask = document.querySelectorAll(".product_for_basket");
+
+        if(e_ask.length < 5)
+        {
+        
         // part1 function
         var parent_tag = this.parentNode , 
             size =  parent_tag.childNodes[1].value , 
@@ -337,6 +339,22 @@ var button_take_b = documen_t.getElementsByClassName("take_basket");
                             }
                         }   
                     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               }//if
             } // end for
 
 
@@ -455,7 +473,15 @@ function move()
     else
     {
         block_f_i.className = "block_f";
-        number_bl_f.innerHTML = anchor.children.length;
+
+        if(anchor.children.length == 1)
+        {
+            number_bl_f.innerHTML = (anchor.children.length) + " товар";
+        }
+        else
+        {
+            number_bl_f.innerHTML = (anchor.children.length) + " товара";
+        }
 
     }
 
@@ -511,38 +537,46 @@ function move()
 
 // checkbox _big_block
 
+function dinamic_func_for_check()
+{   
+                var che_block = documen_t.querySelectorAll(".checkbox") , 
+                    block_f_id = documen_t.querySelectorAll(".products");
+                    
 
 
-var che_block = documen_t.querySelectorAll(".checkbox") , 
-    block_f_id = documen_t.querySelectorAll(".products");
-    
-
-    for(var m = 0; m < che_block.length ; m++)
-    {
-        che_block[m].addEventListener("click", func_check_block);
-    }
+                for(var m = 0; m < che_block.length ; m++)
+                {
+                    che_block[m].addEventListener("click", func_check_block);
+                }
 
 
-function func_check_block()
-{
-    for(var i = 0 ; i < block_f_id.length ; i++)
-    {
-        if(this.parentNode.id == block_f_id[i].getAttribute('name'))
-        {
-            if(this.checked)
+            function func_check_block()
             {
-                block_f_id[i].className = "products";
+
+
+                for(var i = 0 ; i < block_f_id.length ; i++)
+                {
+                    if(this.parentNode.id == block_f_id[i].getAttribute('name'))
+                    {
+                        if(this.checked)
+                        {
+                            block_f_id[i].className = "products";
+                        }
+                        else
+                        {
+                            block_f_id[i].className = "ds";
+                        }
+                    }
+                }
+
+
+
             }
-            else
-            {
-                block_f_id[i].className = "ds";
-            }
-        }
-    }
-
-
-
 }
+
+setInterval(dinamic_func_for_check, 10);
+
+
 
 
 
@@ -606,12 +640,432 @@ function rest_button()
 
         // label.appendChild(cr_span);
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// show block products all
+
+
+function dinamic_show()
+{
+
+
+var bnt_check_all = documen_t.querySelector("#bnt_check_all");
+
+    bnt_check_all.addEventListener("click", func_check);
+    function func_check()
+    {
+        var ds = documen_t.querySelectorAll(".ds");
+
+
+        for(var i = 0 ; i < ds.length; i++)
+        {
+            ds[i].className = "products";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // show block products all
+            bnt_check_all.addEventListener("click", check_l);
+
+
+            function check_l()
+            {
+                // mass
+                var mass_t = ["MEN", "WOMEN", "CHILDREN"] , 
+                    mass_number = 0;
+
+
+
+
+
+                var menu_selection = documen_t.querySelector(".menu_selection");
+                    // Part 1
+                    menu_selection.children[0].remove();
+                    // Part 2
+
+                    // create functions
+                        function create_element()
+                        {
+                            var label_1 = documen_t.createElement("label") , 
+                                label_2 = documen_t.createElement("label") ,
+                                label_3 = documen_t.createElement("label") ,
+                                button = documen_t.createElement("button") ,
+                                div_el = documen_t.createElement("div") , 
+                                div_el_2 = documen_t.createElement("div");
+
+                                // id
+
+                                label_1.setAttribute("id", "che_ck1"); 
+                                label_2.setAttribute("id", "che_ck2"); 
+                                label_3.setAttribute("id", "che_ck3"); 
+                                button.setAttribute("id", "bnt_check_all");
+
+                                // class
+
+                                label_1.setAttribute("class", "upper"); 
+                                label_2.setAttribute("class", "upper"); 
+                                label_3.setAttribute("class", "upper"); 
+                                button.setAttribute("class", "white_t upper right");
+                                div_el.setAttribute("class", "clear");
+                                div_el_2.setAttribute("class", "mr_l_34");
+
+
+                                // skelet
+
+                                div_el_2.appendChild(label_1);
+                                div_el_2.appendChild(label_2);
+                                div_el_2.appendChild(label_3);
+                                div_el_2.appendChild(button);
+                                div_el_2.appendChild(div_el);
+
+
+                                // inner
+
+                                button.innerHTML = "See all products";
+
+                                // end func;
+
+                                return div_el_2;
+
+                        }
+
+                        function cild()
+                        {
+
+                            var input = documen_t.createElement("input") , 
+                                span_1 = documen_t.createElement("span") ,
+                                span_2 = documen_t.createElement("span") , 
+                                mass = [];
+
+                                // class 
+
+                                input.setAttribute("class", "checkbox");
+                                span_1.setAttribute("class", "checkbox-custom");
+                                span_2.setAttribute("class", "label");
+
+                                // name element
+
+                                span_2.style.marginLeft = "8px";
+                               
+
+                                // inner
+                                span_2.innerHTML = mass_t[mass_number];
+
+                                mass_number++;
+
+                                if(mass_number == 3)
+                                {
+                                    mass_number = 0 ;
+                                }
+
+
+                                // checked
+
+                                input.setAttribute("checked", "checked");
+
+                                //type 
+
+                                 input.setAttribute("type", "checkbox");
+
+                                // name
+                                input.setAttribute("name", "checkbox-test");
+
+                                mass.push(input);
+                                mass.push(span_1);
+                                mass.push(span_2);
+
+                                return mass;
+
+
+
+                        }
+
+                        
+                         function for_function ()
+                         {
+                            var element = create_element(); 
+                                
+                                  for(var i = 0 ; i < element.children.length ; i++)
+                                    {
+                                        // add new element func child()
+                                        var ch = cild();
+                                        if(element.children[i].nodeName == "LABEL")
+                                        {
+                                            for(var n = 0 ; n < ch.length; n++)
+                                            {
+                                                element.children[i].appendChild(ch[n]);   
+                                            }
+                                        };
+                                    };
+
+                                    return element;
+
+                         }  
+
+                         menu_selection.appendChild(for_function());
+
+                        
+            }
+
+
+
+
 
 
 }
 
 
+setInterval(dinamic_show, 10);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++big functions+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+// dinamic block products
+
+var left = 0;
+
+function func_dinamic_r()
+{
+    var element = this.parentNode.parentNode.parentNode.children[1] ,
+    dvi = func_dv(element);
+    dvi_len_r(dvi, element);
+}
+
+
+
+
+
+
+
+
+
+
+function func_dinamic_l()
+{
+    var element = this.parentNode.parentNode.parentNode.children[1] ,
+    dvi = func_dv(element);
+    dvi_len_r(dvi, element);
+}
+
+
+
+
+
+
+
+
+
+// regX
+
+function func_dv(element){
+
+    var element_ch = element.children ,
+    element_ch_str = "" ,
+    reg = /def_product/gi;
+
+    for(var i = 0 ; i < element_ch.length ; i++)
+    {
+         element_ch_str += element_ch[i].getAttribute("class");
+    }
+
+    return element_ch_str.match(reg);
+
+}
+
+
+
+
+
+
+
+// ask_r
+
+function dvi_len_r(dvi , element)
+        {
+            switch(dvi.length)
+            {
+                case 4:{
+                     left = left - 385;
+
+                    if(left < - 385)
+                        {
+                            left = 0;
+                        }
+
+                    element.style.left = left + "px";
+                };
+                break;
+            }
+        }
+
+
+// ask_r
+
+ function dvi_len_r(dvi, element)
+    {
+         switch(dvi.length)
+            {
+                case 4:{
+                  
+                    left = left + 385;
+
+
+                    if(left >=  385)
+                        {
+                            left = -385;
+                        }
+
+                    element.style.left = left + "px";
+                };
+                break;
+            }
+    }
+
+
+
+
+
+
+
+// start function
+
+var dinamic_block_left = documen_t.querySelectorAll(".dinamic_block_left") ,
+    dinamic_block_right = documen_t.querySelectorAll(".dinamic_block_right");
+
+    for(var i = 0 ; i < dinamic_block_right.length;i++)
+    {
+       dinamic_block_right[i].onclick = func_dinamic_r;
+       dinamic_block_left[i].onclick = func_dinamic_l;
+    }
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++big functions+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
+// scroll
+
+
+
+// window.scrollTo()
+    var scrolled ,
+        timer;
+
+
+    document.getElementById("block_f_i").onclick = function()
+    {
+        scrolled = window.pageYOffset;
+        // window.scrollTo(0,0);
+        scrollToTop();
+    };
+
+    function scrollToTop()
+    {
+        if(scrolled > 0)
+        {
+            window.scrollTo(0, scrolled);
+            scrolled = scrolled - 40;// скорость прокрутки 
+            timer = setTimeout(scrollToTop, 10);
+        }
+        else
+        {
+            clearTimeout(timer);
+            window.scrollTo(0,0);
+        }
+    }
 
 
 
